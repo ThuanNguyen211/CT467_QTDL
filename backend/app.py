0be2a79c6b6ea49c5a0f33640f6b1247fac47ab7
Truyennm0167 <truyentruyen113113@gmail.com>
2025-07-14 16:36:42 +0700

    Thêm tính năng 9. Hiển thị ds còn trống của bs vào khung giờ

--- backend/app.py
+++ backend/app.py
@@ -3,11 +3,10 @@ from flask_cors import CORS
 from routes.doctor_routes import doctor_bp
 from routes.booking_routes import booking_bp
 from routes.medical_exam_routes import medical_exam_bp
-
 from routes.user_routes import user_bp
 from routes.prescription_routes import prescription_bp
-
 from routes.specialty_routes import specialty_bp
+from routes.features import appointment_bp
 
 
 app = Flask(__name__)
@@ -20,6 +19,7 @@ app.register_blueprint(medical_exam_bp)
 app.register_blueprint(user_bp)
 app.register_blueprint(prescription_bp)
 app.register_blueprint(specialty_bp)
+app.register_blueprint(appointment_bp)
 
 if __name__ == '__main__':
     app.run(port=5000)
