aff36a22014ed6f9769c159402a99aa54ac8def9
ThuanNguyen211 <nmthuan211@gmail.com>
2025-07-16 17:07:34 +0700

    done login

--- backend/db.py
+++ backend/db.py
@@ -4,6 +4,6 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="admin",
+        password="123456",
         database="phong_kham"
     )
