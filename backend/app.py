eb605781424ba67a5558eadc4cd8a1455ae4541d
ThuanNguyen211 <nmthuan211@gmail.com>
2025-07-16 13:59:43 +0700

    update routeroute

--- backend/app.py
+++ backend/app.py
@@ -3,11 +3,11 @@ from flask_cors import CORS
 from routes.doctor_routes import doctor_bp
 from routes.booking_routes import booking_bp
 from routes.medical_exam_routes import medical_exam_bp
-
 from routes.user_routes import user_bp
 from routes.prescription_routes import prescription_bp
-
 from routes.specialty_routes import specialty_bp
+from routes.medicine_routes import medicine_bp
+from routes.patient_routes import patient_bp
 
 
 app = Flask(__name__)
@@ -20,6 +20,8 @@ app.register_blueprint(medical_exam_bp)
 app.register_blueprint(user_bp)
 app.register_blueprint(prescription_bp)
 app.register_blueprint(specialty_bp)
+app.register_blueprint(medicine_bp)
+app.register_blueprint(patient_bp)
 
 if __name__ == '__main__':
     app.run(port=5000)
